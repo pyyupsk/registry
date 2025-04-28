b295103a0b89601108765b084c4eb165118a7eec
pyyupsk <pyyupsk@proton.me>
2025-04-28 13:27:51 +0700

    feat: init commit with setup files

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import prettier from "eslint-plugin-prettier/recommended";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = tseslint.config(
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslint.configs.recommended,
+  perfectionist.configs["recommended-natural"],
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          fixStyle: "separate-type-imports",
+          prefer: "type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  prettier,
+);
+
+export default eslintConfig;
