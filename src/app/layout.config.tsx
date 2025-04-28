f29f7ec3950141dec15214aecc0153e1e587f472
pyyupsk <pyyupsk@proton.me>
2025-04-28 14:10:36 +0700

    feat: integrate fumadocs for documentation

---
+++ src/app/layout.config.tsx
@@ -0,0 +1,28 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  links: [
+    {
+      active: "nested-url",
+      text: "Documentation",
+      url: "/docs",
+    },
+  ],
+  nav: {
+    title: (
+      <>
+        <svg aria-label="Logo" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+          <circle cx={12} cy={12} fill="currentColor" r={12} />
+        </svg>
+        My App
+      </>
+    ),
+  },
+};
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,9 +1,12 @@
+import { RootProvider } from "fumadocs-ui/provider";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      {children}
-    </ThemeProvider>
+    <RootProvider>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        {children}
+      </ThemeProvider>
+    </RootProvider>
   );
 }
