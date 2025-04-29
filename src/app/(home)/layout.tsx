ae9305c72dd29a91297c43de6d0081da376b44fe
pyyupsk <pyyupsk@proton.me>
2025-04-29 11:53:26 +0700

    refactor: update metadata generation in layout files for consistency

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,12 +4,6 @@ import { baseOptions } from "@/app/layout.config";
 import { generateMetadata } from "@/lib/metadata";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 
-export const metadata = generateMetadata({
-  description:
-    "A growing collection of beautifully designed, ready-to-use components for your next project.",
-  title: "fasu.dev | shadcn/ui components collections.",
-});
-
 export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
